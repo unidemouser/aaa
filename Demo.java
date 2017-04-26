285ada9f43c265f1dcb9f72b2ed1de758654e64a
unidemouser <rahul.niraula@yahoo.com>
2017-04-26 11:21:50 +0545

    added comments

--- Demo.java
+++ Demo.java
@@ -3,6 +3,7 @@ package edu.unicampus;
 public class Demo{
 
     public static void main(String[] args){
-        System.out.println("Thi si entry point");
+        System.out.println("Thi si entry point"); 
+	//this is comment
     }
 }
